db1ca1fcb348e0701c65f7116b686beb5ec3529a
SunDust23 <julik23@bk.ru>
2022-11-05 19:27:36 +0500

    Get all text files by path

--- Log parser/Program.cs
+++ Log parser/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Linq;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        string path = args[0];
+
+        List<string> logFiles = new();
+
+
+        if (Directory.Exists(path))
+        {
+            logFiles = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+                .Where(s => s.EndsWith(".log") || s.EndsWith(".txt")).ToList();
+        }
+        else if (File.Exists(path))
+        {
+            logFiles.Add(path);
+        }
+        else
+        {
+            throw new Exception("File not exists");
+        }
+
+        Console.WriteLine(logFiles.Aggregate((A, B) => A + '\n' + B));
+
+        foreach (string logFile in logFiles)
+        {
+            try
+            {
+               File.ReadAllText(logFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+    }
+}
